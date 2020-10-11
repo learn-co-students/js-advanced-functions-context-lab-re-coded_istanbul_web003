cd98786701aed1b56b87e01835e8bd537f8ffcfb
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-11 16:41:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 /* Your Code Here */
+const createEmployeeRecord = (arr) => {
+    const obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj
+}
+
+const createEmployeeRecords = (arrs) => {
+    return arrs.map(arr => createEmployeeRecord(arr))
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    });
+
+    return this;
+};
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: date,
+    });
+
+    return this;
+};
+
+function hoursWorkedOnDate(dateStamp){
+    const checkIn = this.timeInEvents.find(time => time.date === dateStamp)
+    const checkOut= this.timeOutEvents.find(time => time.date === dateStamp)
+    return (checkOut.hour - checkIn.hour) / 100 
+}
+
+function wagesEarnedOnDate(dateStamp){
+    const payOwed = hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour;
+    return parseFloat(payOwed.toString());
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +69,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find((e) => {
+      return e.firstName === firstName;
+    });
+  };
+
+function calculatePayroll(records) {
+    return records.reduce((acc, cur)=> acc + allWagesFor.call(cur),0)
 }
