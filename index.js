b3cc6a159c2c50f9ddc0efd6c0ae12c2bc7dbf77
betul <bet.er@hotmail.com>
2020-10-31 15:40:14 +0300

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,58 @@
  for you to use if you need it!
  */
 
-let allWagesFor = function () {
+function createEmployeeRecord(employeeData){
+    const obj = {
+        firstName: employeeData[0], 
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return obj;
+}
+
+function createEmployeeRecords(employeeData){
+    return employeeData.map(singleData => createEmployeeRecord(singleData))
+}
+
+function createTimeInEvent(date){
+    const splittedDate = date.split(" ");
+    const data = {
+        type: "TimeIn",
+        date: splittedDate[0],
+        hour: parseInt(splittedDate[1])
+    }
+    this.timeInEvents.push(data);
+    return this;
+}
+
+function createTimeOutEvent(date){
+    const splittedDate = date.split(" ");
+    const data = {
+        type: "TimeOut",
+        date: splittedDate[0],
+        hour: parseInt(splittedDate[1])
+    }
+    this.timeOutEvents.push(data);
+    return this;
+}
+
+function hoursWorkedOnDate(givenDate){
+    const timeIn = this.timeInEvents.find(i => i.date === givenDate)
+    const timeOut = this.timeOutEvents.find(o => o.date === givenDate)
+    console.log(timeOut.hour - timeIn.hour)
+    const result = (timeOut.hour - timeIn.hour) / 100;
+    return result;
+}
+
+function wagesEarnedOnDate(givenDate){
+   const totalHour = hoursWorkedOnDate.call(this,givenDate);
+   return this.payPerHour*totalHour
+}
+
+let allWagesFor = function (){
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -19,4 +70,18 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    for(const person of srcArray){
+        if(person.firstName === firstName){
+            return person;
+        } else{
+            return undefined
+        }
+    }
+}
+
+function calculatePayroll(employeeData){
+    return employeeData.reduce((acc, curr) => acc + allWagesFor.call(curr), 0);
 }
